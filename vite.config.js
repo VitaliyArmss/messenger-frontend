65e9b3713172a3f87f7a9717a3456a6e9188e9d9
armos <armosother@gmail.com>
2026-09-23 15:59:57 +0700

    new domen

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/messengerclient/',
+    base: '/messenger-frontend/',
     server: {
         proxy: {
             '/api': {
